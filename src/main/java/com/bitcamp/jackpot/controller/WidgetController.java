0880ab8c6d1a5184fbc84379a07d8bb0ef5e09f5
DDolram <whtjdwkab@naver.com>
2024-10-12 19:39:03 +0900

    Working on payment API controller

--- src/main/java/com/bitcamp/jackpot/controller/WidgetController.java
+++ src/main/java/com/bitcamp/jackpot/controller/WidgetController.java
@@ -1,7 +1,52 @@
+/*
 package com.bitcamp.jackpot.controller;
 
 // toss 결제 컨트롤러
+
+import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class WidgetController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @PostMapping("/confirm")
+    public ResponseEntity<JSONObject> confirmPayment(@RequestBody String jsonBody) throws Exception {
+
+        JSONParser parser = new JSONParser();
+        String orderId;
+        String amount;
+        String paymentKey;
+        try{
+            // 클라이언트에서 받은 JSON 요청 바디를 파싱
+            JSONObject requestData = (JSONObject) parser.parse(jsonBody);
+            paymentKey = (String) requestData.get("paymentKey");
+            orderId = (String) requestData.get("orderId");
+            amount = (String) requestData.get("amount");
+        }catch (ParseException e){
+            throw new RuntimeException(e);
+        }
+        JSONObject  obj = new JSONObject();
+        obj.put("orderId", orderId);
+        obj.put("amount", amount);
+        obj.put("paymentKey", paymentKey);
+
+        // 토스페이먼츠 API 시크릿 키 설정
+        String widgetSecretKry = "test"
+    }
+
 
 
 }
+ */
