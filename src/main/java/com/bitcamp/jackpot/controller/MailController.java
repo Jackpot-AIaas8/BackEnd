ed00ac9c1a67f0a5d8ad01ec84b4ceff05f5ee7d
DDolram <whtjdwkab@naver.com>
2024-10-15 21:56:23 +0900

    Fix order-related code issues

--- src/main/java/com/bitcamp/jackpot/controller/MailController.java
+++ src/main/java/com/bitcamp/jackpot/controller/MailController.java
@@ -1,34 +1,34 @@
-package com.bitcamp.jackpot.controller;
-
-import com.bitcamp.jackpot.service.MailService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-
-@RestController
-@RequiredArgsConstructor
-public class MailController {
-
-    private final MailService mailService;
-
-
-    @GetMapping("/sendEmail")
-    public void sendSimpleMailMessage(@RequestParam String email) {
-        mailService.sendSimpleMailMessage(email);
-
-    }
-
-    @GetMapping("/checkVerificationCode")
-    public ResponseEntity<String> checkVerificationCode(@RequestParam String email, @RequestParam String code) {
-        boolean isVerified = mailService.checkVerificationCode(email, code);
-
-        if (isVerified) {
-            return ResponseEntity.ok("인증 되었습니다.");
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("인증코드가 잘못 되었습니다.");
-        }
-    }
-
-}
+//package com.bitcamp.jackpot.controller;
+//
+//import com.bitcamp.jackpot.service.MailService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class MailController {
+//
+//    private final MailService mailService;
+//
+//
+//    @GetMapping("/sendEmail")
+//    public void sendSimpleMailMessage(@RequestParam String email) {
+//        mailService.sendSimpleMailMessage(email);
+//
+//    }
+//
+//    @GetMapping("/checkVerificationCode")
+//    public ResponseEntity<String> checkVerificationCode(@RequestParam String email, @RequestParam String code) {
+//        boolean isVerified = mailService.checkVerificationCode(email, code);
+//
+//        if (isVerified) {
+//            return ResponseEntity.ok("인증 되었습니다.");
+//        } else {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("인증코드가 잘못 되었습니다.");
+//        }
+//    }
+//
+//}
--- src/main/java/com/bitcamp/jackpot/service/MailService.java
+++ src/main/java/com/bitcamp/jackpot/service/MailService.java
@@ -1,72 +1,72 @@
-package com.bitcamp.jackpot.service;
-
-import com.bitcamp.jackpot.dto.MailDTO;
-import com.bitcamp.jackpot.jwt.RedisUtil;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Random;
-
-
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class MailService {
-
-    private final JavaMailSender javaMailSender;
-    private final RedisUtil redisUtil;
-
-
-    private String createCode() throws NoSuchAlgorithmException {
-        int length = 6;
-
-            Random random = SecureRandom.getInstanceStrong();
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < length; i++) {
-                builder.append(random.nextInt(10));
-            }
-            return builder.toString();
-    }
-    public boolean checkVerificationCode(String email, String code) {
-        try {
-
-            if (redisUtil.isValueEqual(email,code)){
-                redisUtil.delete(email);
-                return true;
-            }else return false;
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return false;
-        }
-    }
-
-    public void sendSimpleMailMessage(String email) {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-
-        try {
-            // 메일을 받을 수신자 설정
-            simpleMailMessage.setTo(email);
-            // 메일의 제목 설정
-            simpleMailMessage.setSubject("ppyppy 인증번호");
-            // 메일의 내용 설정
-            String secretCode=createCode();
-            simpleMailMessage.setText("인증번호 : "+secretCode);
-
-            javaMailSender.send(simpleMailMessage);
-            redisUtil.set(email,secretCode,3);
-            
-            log.info("메일 발송 성공!");
-        } catch (Exception e) {
-            log.info("메일 발송 실패!");
-            throw new RuntimeException(e);
-        }
-
-    }
-}
+//package com.bitcamp.jackpot.service;
+//
+//import com.bitcamp.jackpot.dto.MailDTO;
+//import com.bitcamp.jackpot.jwt.RedisUtil;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//
+//import java.security.NoSuchAlgorithmException;
+//import java.security.SecureRandom;
+//import java.util.Random;
+//
+//
+//@Service
+//@Slf4j
+//@RequiredArgsConstructor
+//public class MailService {
+//
+////    private final JavaMailSender javaMailSender;
+//    private final RedisUtil redisUtil;
+//
+//
+//    private String createCode() throws NoSuchAlgorithmException {
+//        int length = 6;
+//
+//            Random random = SecureRandom.getInstanceStrong();
+//            StringBuilder builder = new StringBuilder();
+//            for (int i = 0; i < length; i++) {
+//                builder.append(random.nextInt(10));
+//            }
+//            return builder.toString();
+//    }
+//    public boolean checkVerificationCode(String email, String code) {
+//        try {
+//
+//            if (redisUtil.isValueEqual(email,code)){
+//                redisUtil.delete(email);
+//                return true;
+//            }else return false;
+//        } catch (Exception e) {
+//            log.error(e.getMessage());
+//            return false;
+//        }
+//    }
+//
+//    public void sendSimpleMailMessage(String email) {
+//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+//
+//        try {
+//            // 메일을 받을 수신자 설정
+//            simpleMailMessage.setTo(email);
+//            // 메일의 제목 설정
+//            simpleMailMessage.setSubject("ppyppy 인증번호");
+//            // 메일의 내용 설정
+//            String secretCode=createCode();
+//            simpleMailMessage.setText("인증번호 : "+secretCode);
+//
+//            javaMailSender.send(simpleMailMessage);
+//            redisUtil.set(email,secretCode,3);
+//
+//            log.info("메일 발송 성공!");
+//        } catch (Exception e) {
+//            log.info("메일 발송 실패!");
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
+//}
