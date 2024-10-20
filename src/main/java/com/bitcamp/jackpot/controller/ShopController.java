cdf66933c27a8e930ad8095a8439cc388ff4b888
김민준 <kimmj9703@gmail.com>
2024-10-20 19:54:37 +0900

    merge

--- src/main/java/com/bitcamp/jackpot/controller/ShopController.java
+++ src/main/java/com/bitcamp/jackpot/controller/ShopController.java
@@ -10,6 +10,8 @@ import com.bitcamp.jackpot.service.MemberService;
 import com.bitcamp.jackpot.service.ShopService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,11 +30,22 @@ public class ShopController {
         return shopService.findOne(shopId);
     }
 
+//    @GetMapping("/findList")
+//    public PageResponseDTO<ShopDTO> findList(PageRequestDTO pageRequestDTO) {
+////        log.info(pageRequestDTO);
+//        return shopService.findList(pageRequestDTO);
+//    }
+
     @GetMapping("/findList")
-    public PageResponseDTO<ShopDTO> findList(PageRequestDTO pageRequestDTO) {
-//        log.info(pageRequestDTO);
-        return shopService.findList(pageRequestDTO);
+    public ResponseEntity<PageResponseDTO<ShopDTO>> findList(PageRequestDTO pageRequestDTO) {
+        // log.info(pageRequestDTO);
+        PageResponseDTO<ShopDTO> pageResponseDTO = shopService.findList(pageRequestDTO);
+        log.info(pageResponseDTO);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(pageResponseDTO);
     }
+
 
     @GetMapping("/findOrderList")
     public List<OrdersDTO> findOrderList(PageRequestDTO pageRequestDTO) {
